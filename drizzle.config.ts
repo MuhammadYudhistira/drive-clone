9012a93e2c1c9fd7c271a22954cebae31502f82e
Muhammad Yudhistira <myudhistira07@gmail.com>
2025-04-06 23:33:42 +0700

    Database Added

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,16 @@
-import { type Config } from "drizzle-kit";
-
+import { defineConfig } from "drizzle-kit";
 import { env } from "~/env";
 
-export default {
+export default defineConfig({
+  dialect: "singlestore",
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    host: "svc-3482219c-a389-4079-b18b-d50662524e8a-shared-dml.aws-virginia-6.svc.singlestore.com",
+    user: "muhammad-c1f4d",
+    password: "IaUMeUdoEmTVID902PNUweUgejAZYhZq",
+    port: 3333,
+    database: "DRIVE_VELLICHOR_DB",
+    ssl: {},
   },
   tablesFilter: ["drive-clone_*"],
-} satisfies Config;
+});
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,19 +1,9 @@
-import { createClient, type Client } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
-
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import mysql from "mysql2/promise";
+import { drizzle } from "drizzle-orm/singlestore";
 import { env } from "~/env";
-import * as schema from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
-const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
-};
-
-export const client =
-  globalForDb.client ?? createClient({ url: env.DATABASE_URL });
-if (env.NODE_ENV !== "production") globalForDb.client = client;
-
-export const db = drizzle(client, { schema });
+const pool = mysql.createPool(env.SINGLESTORE_CONNECTION_STRING as string);
+const db = drizzle({ client: pool });
